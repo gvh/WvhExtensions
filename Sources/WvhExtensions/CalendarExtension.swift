07c7b743267de3dbf5e6d33ba8738bfd6524fc2c
Gardner von Holt <gardner@wvh.com>
2022-10-15 14:51:36 -0500

    Add endOfDay

---
+++ Sources/WvhExtensions/CalendarExtension.swift
@@ -0,0 +1,16 @@
+//
+//  CalendarExtension.swift
+//  
+//
+//  Created by Gardner von Holt on 10/15/22.
+//
+
+import Foundation
+
+extension Calendar {
+    func endOfDay(for forDate: Date) -> Date {
+        let oneDay = TimeInterval((24.0 * 60.0 * 60.0) - 1.0)
+        let endDate = self.startOfDay(for: forDate).addingTimeInterval(oneDay)
+        return endDate
+    }
+}
