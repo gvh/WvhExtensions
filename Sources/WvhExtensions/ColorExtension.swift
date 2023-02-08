6a18471922d528715e94c20d8487344ece396cfc
Gardner von Holt <gardner@wvh.com>
2023-02-08 06:18:41 -0600

    Sortable Colors

---
+++ Sources/WvhExtensions/ColorExtension.swift
@@ -0,0 +1,36 @@
+//
+//  ColorExtension.swift
+//  
+//
+//  Created by Gardner von Holt on 2/8/23.
+//
+
+import SwiftUI
+
+extension Color : Comparable {
+    public static func < (lhs: Color, rhs: Color) -> Bool {
+        let lhsInt: Int = lhs.intForColor()
+        let rhsInt: Int = rhs.intForColor()
+        return lhsInt < rhsInt
+    }
+
+    private func intForColor() -> Int {
+        var sortOrder: Int
+        switch self {
+        case .gray:
+            sortOrder = 1
+        case .blue:
+            sortOrder = 2
+        case .green:
+            sortOrder = 3
+        case .yellow:
+            sortOrder = 4
+        case .red:
+            sortOrder = 5
+        default:
+            sortOrder = 6
+        }
+        return sortOrder
+    }
+
+}
