ea8f45ce4108a15a13b5a89e4d2688431094b431
Gardner von Holt <gardner@wvh.com>
2023-08-09 19:50:04 -0500

    fix macOS issue

--- Sources/WvhExtensions/UIDeviceExtension.swift
+++ Sources/WvhExtensions/UIDeviceExtension.swift
@@ -5,15 +5,24 @@
 //  Created by Gardner von Holt on 8/21/21.
 //  Copyright © 2021-2023 Gardner von Holt. All rights reserved.
 //
-
+#if os(tvOS) || os(iOS)
 import UIKit
+#endif
 
 public extension UIDevice {
     static var isIPad: Bool {
+        #if os(tvOS) || os(iOS)
         UIDevice.current.userInterfaceIdiom == .pad
+        #else
+        false
+        #endif
     }
 
     static var isIPhone: Bool {
+#if os(tvOS) || os(iOS)
         UIDevice.current.userInterfaceIdiom == .phone
+#else
+        false
+#endif
     }
 }
