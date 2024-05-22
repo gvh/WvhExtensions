160c225bfabc4ac40ab8f82a489996200b6ffefd
Gardner von Holt <gardner@wvh.com>
2024-05-22 18:43:35 -0500

    new stuff from movies

--- Sources/WvhExtensions/ArrayExtension.swift
+++ Sources/WvhExtensions/ArrayExtension.swift
@@ -9,6 +9,12 @@
 import Foundation
 
 public extension Array where Element: Hashable {
+    func diff(from other: [Element]) -> [Element] {
+        let thisSet = Set(self)
+        let otherSet = Set(other)
+        return Array(thisSet.symmetricDifference(otherSet))
+    }
+
     func removingDuplicates() -> [Element] {
         var addedDict = [Element: Bool]()
 
