90957516e3babee2b7d97cc8b4432d10b4592517
Gardner von Holt <gardner@wvh.com>
2024-11-06 02:30:13 -0600

    New Array remove, remove experiments

--- Sources/WvhExtensions/ArrayExtension.swift
+++ Sources/WvhExtensions/ArrayExtension.swift
@@ -31,3 +31,12 @@ public extension Array where Element: Hashable {
         return !self.isEmpty
     }
 }
+
+extension Array where Element: Equatable {
+
+    // Remove first collection element that is equal to the given `object`:
+    mutating func remove(object: Element) {
+        guard let index = firstIndex(of: object) else {return}
+        remove(at: index)
+    }
+    }
