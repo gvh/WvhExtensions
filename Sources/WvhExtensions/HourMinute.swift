514d8fb0c398941565ec6db26f7b6661f890a62a
Gardner von Holt <gardner@wvh.com>
2025-12-14 14:45:51 -0600

    fix visibility

--- Sources/WvhExtensions/HourMinute.swift
+++ Sources/WvhExtensions/HourMinute.swift
@@ -61,7 +61,7 @@ public struct HourMinute: Codable, Comparable, Sendable, Equatable {
         return lhs == rhsHourMinute
     }
 
-    func toString() -> String {
+    public func toString() -> String {
         String(format: "%02d:%02d", hour, minute)
     }
 }
